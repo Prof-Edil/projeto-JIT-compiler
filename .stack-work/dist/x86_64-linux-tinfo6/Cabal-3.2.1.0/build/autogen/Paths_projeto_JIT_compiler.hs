b9451979a7e3201a4ff1e05655e077fbef49141a
ivan-cunha <ivanfilho299@gmail.com>
2021-05-19 19:27:53 -0300

    codegen added

---
+++ .stack-work/dist/x86_64-linux-tinfo6/Cabal-3.2.1.0/build/autogen/Paths_projeto_JIT_compiler.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_projeto_JIT_compiler (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/mnt/c/Users/ivanc/Desktop/UnB/Disciplinas/T\243picos - Edil/projeto-JIT-compiler/.stack-work/install/x86_64-linux-tinfo6/de467e15908489be3fd5440ece2557b7087ad65831fbef331fc99e813d18cf9f/8.10.4/bin"
+libdir     = "/mnt/c/Users/ivanc/Desktop/UnB/Disciplinas/T\243picos - Edil/projeto-JIT-compiler/.stack-work/install/x86_64-linux-tinfo6/de467e15908489be3fd5440ece2557b7087ad65831fbef331fc99e813d18cf9f/8.10.4/lib/x86_64-linux-ghc-8.10.4/projeto-JIT-compiler-0.1.0.0-8tzl6Nbp2nOBtvF9bCh4Rl"
+dynlibdir  = "/mnt/c/Users/ivanc/Desktop/UnB/Disciplinas/T\243picos - Edil/projeto-JIT-compiler/.stack-work/install/x86_64-linux-tinfo6/de467e15908489be3fd5440ece2557b7087ad65831fbef331fc99e813d18cf9f/8.10.4/lib/x86_64-linux-ghc-8.10.4"
+datadir    = "/mnt/c/Users/ivanc/Desktop/UnB/Disciplinas/T\243picos - Edil/projeto-JIT-compiler/.stack-work/install/x86_64-linux-tinfo6/de467e15908489be3fd5440ece2557b7087ad65831fbef331fc99e813d18cf9f/8.10.4/share/x86_64-linux-ghc-8.10.4/projeto-JIT-compiler-0.1.0.0"
+libexecdir = "/mnt/c/Users/ivanc/Desktop/UnB/Disciplinas/T\243picos - Edil/projeto-JIT-compiler/.stack-work/install/x86_64-linux-tinfo6/de467e15908489be3fd5440ece2557b7087ad65831fbef331fc99e813d18cf9f/8.10.4/libexec/x86_64-linux-ghc-8.10.4/projeto-JIT-compiler-0.1.0.0"
+sysconfdir = "/mnt/c/Users/ivanc/Desktop/UnB/Disciplinas/T\243picos - Edil/projeto-JIT-compiler/.stack-work/install/x86_64-linux-tinfo6/de467e15908489be3fd5440ece2557b7087ad65831fbef331fc99e813d18cf9f/8.10.4/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "projeto_JIT_compiler_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "projeto_JIT_compiler_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "projeto_JIT_compiler_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "projeto_JIT_compiler_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "projeto_JIT_compiler_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "projeto_JIT_compiler_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
